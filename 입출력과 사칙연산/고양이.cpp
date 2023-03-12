b8a3896bb7284d12133ad3024696a92927d8dead
hong0002 <34588777+hong0002@users.noreply.github.com>
2023-03-13 00:15:15 +0900

    10171

---
+++ 입출력과 사칙연산/고양이.cpp
@@ -0,0 +1,11 @@
+/*
+고양이를 출력한다.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "\\    /\\" << endl << " )  ( ')" << endl << "(  /  )" << endl << " \\(__)|";
+	return 0;
+}
