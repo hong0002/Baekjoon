b25446ccd9191b653b45334f3746ea5c14fa3718
hong0002 <34588777+hong0002@users.noreply.github.com>
2023-03-13 00:21:45 +0900

    10172

---
+++ 입출력과 사칙연산/개.cpp
@@ -0,0 +1,11 @@
+/*
+개를 출력한다.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "|\\_/|" << endl << "|q p|   /}" << endl << "( 0 )\"\"\"\\" << endl << "|\"\^\"\`    |" << endl << "||_/=\\\\__|";
+	return 0;
+}
