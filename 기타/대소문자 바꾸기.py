82b38a4187af7df9de25447dc77e42df1f0b8e79
hong0002 <34588777+hong0002@users.noreply.github.com>
2023-04-20 22:36:02 +0900

    2744

---
+++ 기타/대소문자 바꾸기.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#영어 소문자와 대문자로 이루어진 단어를 입력받은 뒤, 대문자는 소문자로, 소문자는 대문자로 바꾸어 출력하는 프로그램을 작성하시오.
+
+
+#첫째 줄에 영어 소문자와 대문자로만 이루어진 단어가 주어진다. 단어의 길이는 최대 100이다.
+
+
+#첫째 줄에 입력으로 주어진 단어에서 대문자는 소문자로, 소문자는 대문자로 바꾼 단어를 출력한다.
+
+word = input()
+changeword = ""
+
+for i in word:
+    if i.islower():
+        changeword += i.upper()
+    else:
+        changeword += i.lower()
+
+print(changeword)
