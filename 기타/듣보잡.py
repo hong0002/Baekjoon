eb80ec1d0df161f34cab5f8a7e9861df660728a9
hong0002 <34588777+hong0002@users.noreply.github.com>
2023-05-10 22:47:53 +0900

    1764

---
+++ 기타/듣보잡.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+#김진영이 듣도 못한 사람의 명단과, 보도 못한 사람의 명단이 주어질 때, 듣도 보도 못한 사람의 명단을 구하는 프로그램을 작성하시오.
+
+
+#첫째 줄에 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M이 주어진다. 이어서 둘째 줄부터 N개의 줄에 걸쳐 듣도 못한 사람의 이름과, N+2째 줄부터 보도 못한 사람의 이름이 순서대로 주어진다. 이름은 띄어쓰기 없이 알파벳 소문자로만 이루어지며, 그 길이는 20 이하이다. N, M은 500,000 이하의 자연수이다.
+
+#듣도 못한 사람의 명단에는 중복되는 이름이 없으며, 보도 못한 사람의 명단도 마찬가지이다.
+
+
+#듣보잡의 수와 그 명단을 사전순으로 출력한다.
+
+import sys
+
+n, m = map(int, input().split())
+if n > m: # n과 m중 작은 값을 n으로 할당
+    n, m = m, n
+
+nmList = []
+nSet = set()
+for i in range(n):
+    nSet.add(sys.stdin.readline().rstrip())
+
+for i in range(m):
+    x = sys.stdin.readline().rstrip()
+    if x in nSet: # x가 nSet안에 존재한다면 nmList에 x추가
+        nmList.append(x)
+
+nmList.sort()
+print(len(nmList))
+for name in nmList:
+    print(name)
