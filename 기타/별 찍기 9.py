bafaba8cdfae13596fcc5695a8ad3f86646f0550
hong0002 <34588777+hong0002@users.noreply.github.com>
2023-05-23 18:07:17 +0900

    2446

---
+++ 기타/별 찍기 9.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+#예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+
+
+#첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+
+#첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
+
+n = int(input())
+
+for i in range(n):
+    print(' '*i, end="")
+    print('*'*((n-i)*2-1))
+
+for i in range(1, n):
+    print(' '*(n-1-i), end="")
+    print('*'*((i)*2+1))
