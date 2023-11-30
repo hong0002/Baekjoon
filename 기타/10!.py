cc2fa194ec607c7c107cdadc1b7991e78f87f954
Park Jihong <34588777+hong0002@users.noreply.github.com>
2023-11-30 20:41:54 +0900

    28352

---
+++ 기타/10!.py
@@ -0,0 +1,13 @@
+# 입력 받기
+N = int(input())
+
+# N! 계산
+factorial = 1
+for i in range(1, N + 1):
+    factorial *= i
+
+# N!초가 몇 주와 동일한지 계산
+weeks = factorial // (7 * 24 * 60 * 60)
+
+# 결과 출력
+print(weeks)
