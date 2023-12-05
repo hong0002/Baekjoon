ad7b00daeb6c937d626bf2309accbd6874117c4f
Park Jihong <34588777+hong0002@users.noreply.github.com>
2023-12-05 15:17:30 +0900

    2776

---
+++ 기타/암기왕.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    note1 = set(map(int, input().split()))
+    m = int(input())
+    note2 = list(map(int, input().split()))
+    for i in note2:
+        print(1) if i in note1 else print(0)
