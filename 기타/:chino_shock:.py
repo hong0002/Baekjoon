0b6ac8ce861f04ac290759d1de6de266556c6739
Park Jihong <34588777+hong0002@users.noreply.github.com>
2024-01-02 12:41:35 +0900

    27310

---
+++ 기타/:chino_shock:.py
@@ -0,0 +1,14 @@
+def calculate_input_difficulty(emoji):
+    length = len(emoji)
+    colons = emoji.count(':')
+    underscores = emoji.count('_')
+    
+    difficulty = length + colons + underscores * 5
+    return difficulty
+
+# 입력 받기
+emoji = input().strip()
+
+# 입력 난이도 계산하고 출력
+difficulty = calculate_input_difficulty(emoji)
+print(difficulty)
