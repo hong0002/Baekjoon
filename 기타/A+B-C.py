fc0be9539b66f9eebc66fda8ac2299af683e97e4
Park Jihong <34588777+hong0002@users.noreply.github.com>
2024-02-05 15:29:59 +0900

    31403

---
+++ 기타/A+B-C.py
@@ -0,0 +1,14 @@
+# 정수 A, B, C를 입력받음
+A = int(input())
+B = int(input())
+C = int(input())
+
+# 수로 생각할 때 A+B-C 계산
+result_numeric = A + B - C
+
+# 문자열로 생각할 때 A+B-C 계산
+result_string = int(str(A) + str(B)) - C
+
+# 결과 출력
+print(result_numeric)
+print(result_string)
