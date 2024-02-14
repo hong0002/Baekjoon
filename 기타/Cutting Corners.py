3af5aa00ed039a2fbd8d9e82b8e56aa35c5e276b
Park Jihong <34588777+hong0002@users.noreply.github.com>
2024-02-14 21:34:06 +0900

    20215

---
+++ 기타/Cutting Corners.py
@@ -0,0 +1,13 @@
+import math
+
+def extra_cutting_effort(w, h):
+    diagonal_length = math.sqrt(w ** 2 + h ** 2)
+    rectangle_cutting_effort = w + h
+    diagonal_cutting_effort = diagonal_length
+    return rectangle_cutting_effort - diagonal_cutting_effort
+
+# 입력 받기
+w, h = map(int, input().split())
+
+# 결과 출력
+print(extra_cutting_effort(w, h))
