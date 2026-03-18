10ed5c9baa013e9b488981553cdb92b525d59667
Park Jihong <34588777+hong0002@users.noreply.github.com>
2026-03-18 10:59:14 +0900

    34161

---
+++ 기타/OO0OO.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1, s2, s3;
+    getline(cin, s1);
+    getline(cin, s2);
+    getline(cin, s3);
+
+    for (int i = 0; i < 10000; i++) {
+        cout << -1 << '\n';
+    }
+    return 0;
+}
