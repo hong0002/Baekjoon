ef96db6a364992e3988bc9d875080716e508fa9c
Park Jihong <34588777+hong0002@users.noreply.github.com>
2026-03-21 23:22:34 +0900

    1235

---
+++ 기타/학생 번호.py
@@ -0,0 +1,12 @@
+n = int(input())
+nums = [input().strip() for _ in range(n)]
+
+length = len(nums[0])
+
+for k in range(1, length + 1):
+    s = set()
+    for num in nums:
+        s.add(num[-k:])
+    if len(s) == n:
+        print(k)
+        break
